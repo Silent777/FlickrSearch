3b03ba2bc0235972c9dffd19701f396e55a51027
Oleksii Iskiv <silentismyfriend@gmail.com>
2018-08-01 15:25:40 +0300

    added pagination

--- src/components/imageSearch/imageSearchService.js
+++ src/components/imageSearch/imageSearchService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const imageSearchGet = (query) => {
+const imageSearchGet = (query, page) => {
     const per_page = 55
     const apiKey = '6b3575d10435de5f010fc941f5eff94a'
     let url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&
@@ -8,7 +8,8 @@ const imageSearchGet = (query) => {
                 tags=${query}&
                 per_page=${per_page}&
                 format=json&
-                nojsoncallback=1`;
+                nojsoncallback=1&
+                page=${page}`;
     return axios.get(url)
 }
 
